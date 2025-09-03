5f8e2679bfd2b3b00a6e3257de13a8b044f289e6
luizbeep <lg.pradoalencar@gmail.com>
2025-09-03 11:19:53 -0300

    Projeto concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Nokia nokia = new Nokia("1111-2222", "Nokia 3310", "1234567890", 512);
+        Iphone iphone = new Iphone("3333-4444", "iPhone 14", "0987654321", 4096);
+
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
